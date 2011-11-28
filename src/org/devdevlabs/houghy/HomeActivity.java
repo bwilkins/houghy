3baf253628f476938349dc37ea4f40187fd08287
Mathew Hartley <matt@route66.sytes.net>
2011-11-28 20:00:05 +1300

    Added a second activity for camera view
    Another view, designed to be a live preview of camera capture.

---
+++ src/org/devdevlabs/houghy/HomeActivity.java
@@ -0,0 +1,33 @@
+package org.devdevlabs.houghy;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class HomeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.home);
+    }
+    
+    @Override
+    public void onStart() {
+    	super.onStart();
+        Button startCamera = (Button) findViewById(R.id.startCamera);
+        startCamera.setOnClickListener(mCorkyListener);
+    }
+    
+ // Create an anonymous implementation of OnClickListener
+    private OnClickListener mCorkyListener = new OnClickListener() {
+        public void onClick(View v) {
+          // do something when the button is clicked
+        	setContentView(R.layout.viewcapture);
+        }
+    };
+
+}
--- src/org/devdevlabs/houghy/ViewCaptureActivity.java
+++ src/org/devdevlabs/houghy/ViewCaptureActivity.java
@@ -3,11 +3,11 @@ package org.devdevlabs.houghy;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class HoughyActivity extends Activity {
+public class ViewCaptureActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.viewcapture);
     }
 }
